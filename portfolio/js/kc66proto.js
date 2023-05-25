9bd61c773c5f8ca627d6f418cd27416aff23ecba
Keith <big66kc@yahoo.com>
2023-05-25 01:03:48 -0600

    working on creating music player

---
+++ portfolio/js/kc66proto.js
@@ -0,0 +1,9 @@
+function kc_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function kc_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
